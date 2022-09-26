6a37d164d7173f46ccc811455ed49ea3b9b5d7fa
192110153venkatasai <113409353+192110153venkatasai@users.noreply.github.com>
2022-09-26 09:39:35 +0530

    Add files via upload

---
+++ grade and avg marks.py
@@ -0,0 +1,15 @@
+sub1=int(input("enter marks obtained in sub1:"))
+sub2=int(input("enter marks obtained in sub2:"))
+sub3=int(input("enter marks obtained in sub3:"))
+avg=(sub1+sub2+sub3)/3
+print("average marks:",avg marks;)
+if avg marks>=90:
+print(grade is a")
+elif avg marks>=80:
+print(grade is b")
+elif avg marks>=70:
+print (grade is c")
+elif avg marks>=60:
+print(grade is d")
+else:
+print("grade is f")
