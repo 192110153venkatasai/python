9100469a9765394b54192526902c62bdc4d44295
192110153venkatasai <113409353+192110153venkatasai@users.noreply.github.com>
2022-09-26 10:00:22 +0530

    vowels

---
+++ vowels.py
@@ -0,0 +1,8 @@
+a=input("enter a string")
+vowels=0
+for i in a:
+    if(i=="A" or i=="E" or i=="I" or i=="O" or i=="U" or
+       i=="a" or i=="e"  or i=="i" or i=="o" or i=="u"):
+        vowels=vowels+1
+print(vowels)
+       
