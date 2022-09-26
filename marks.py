2d51dddb64368a5097956a337ef6402e69234087
192110153venkatasai <113409353+192110153venkatasai@users.noreply.github.com>
2022-09-26 10:41:20 +0530

    Add files via upload

---
+++ marks.py
@@ -0,0 +1,15 @@
+print("Enter Marks Obtained in 4 Subjects: ")
+mOne = int(input("enter the marks in python:"))
+mTwo = int(input("enter the marks in c programming:"))
+mThree = int(input("enter the marks in mathematics:"))
+mFour = int(input("enter the marks in pysics:"))
+
+sum = mOne+mTwo+mThree+mFour
+avg = sum
+perc = (sum/400)*100
+
+print(end="Average Mark = ")
+print(avg)
+print(end="aggregate = ")
+print(agg)
+
